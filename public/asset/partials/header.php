af65bb266e82fcc0faf527098d499ac163e90a8c
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-01-17 15:43:47 +0100

    ADD:STYLE

--- public/asset/partials/header.php
+++ public/asset/partials/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fight</title>
     <link rel="stylesheet" href="./asset/styles/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <script defer src="../script/script.js"></script>
 </head>
 
--- public/home.php
+++ public/home.php
@@ -7,26 +7,24 @@ $monsterRepo = new MonsterRepository;
 $fightManager = new FightsManager;
 
 ?>
+<main>
+    <section>
+        <form action="../src/process/add_hero.php" method="POST" class="card">
+            <label for="name">Nom du Héros:</label>
+            <input type="text" id="name" name="name" required><br><br>
 
-<section>
-<form action="../src/process/add_hero.php" method="POST">
-    <label for="name">Nom du Héros:</label>
-    <input type="text" id="name" name="name" required><br><br>
+            <label for="specialSkill">Compétence Spéciale:</label>
+            <select id="specialSkill" name="specialSkill" required>
+                <option value="heal">Heal</option>
+                <option value="buff_attack">Buff d'attaque</option>
+            </select><br><br>
 
-    <label for="specialSkill">Compétence Spéciale:</label>
-    <select id="specialSkill" name="specialSkill" required>
-        <option value="heal">Heal</option>
-        <option value="buff_attack">Buff d'attaque</option>
-    </select><br><br>
+            <input type="submit" value="Ajouter le Héros">
+        </form>
+    </section>
+</main>
 
-    <input type="submit" value="Ajouter le Héros">
-</form>
-</section>
-<!-- $fightManager->displayHero() -->
 
 <?php
 require_once './asset/partials/footer.php'
 ?>
-<!-- <a href="https://pokemondb.net/pokedex/charizard"><img src="https://img.pokemondb.net/sprites/black-white/normal/charizard.png" alt="Charizard"></a>
- <a href="https://pokemondb.net/pokedex/mewtwo"><img src="https://img.pokemondb.net/sprites/black-white/normal/mewtwo.png" alt="Mewtwo"></a>
-<a href="https://pokemondb.net/pokedex/blastoise"><img src="https://img.pokemondb.net/sprites/black-white/normal/blastoise.png" alt="Blastoise"></a> -->
