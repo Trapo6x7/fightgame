9e8235ebe95fc0c613af338da7747582241d9efc
Your Name <matoufdanslezoo@gmail.com>
2025-02-18 09:47:27 +0100

    ADD : FAVICON CURSOR

--- public/asset/partials/header.php
+++ public/asset/partials/header.php
@@ -7,6 +7,7 @@
     <title>Fight</title>
     <link rel="stylesheet" href="./asset/styles/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="./asset/imgs/icon.png">
     <script defer src="./script/script.js"></script>
 </head>
 
